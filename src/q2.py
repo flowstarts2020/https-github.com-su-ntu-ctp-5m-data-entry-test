3fac5e03f5adcbd0dd4cf3f74c2c584aa595dec7
flowstarts2020 <69182919+flowstarts2020@users.noreply.github.com>
2024-11-05 16:04:54 +0800

    Update q2.py

--- src/q2.py
+++ src/q2.py
@@ -5,14 +5,13 @@ def find_and_replace(lst, find_val, replace_val):
     - lst must be a list.
     - Return the modified list.
     """
-    return
-if not isinstance(lst, list):
+    if not isinstance(lst, list):
         return "Error: lst must be a list."
 
     modified_list = [replace_val if item == find_val else item for item in lst]
 
     return modified_list
-
+    
 # Task 2
 # Invoke the function "find_and_replace" using the following scenarios:
 # - [1, 2, 3, 4, 2, 2], 2, 5
